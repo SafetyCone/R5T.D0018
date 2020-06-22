717c4b157ac3243cdbefa0d71ef6b4e4a4048826
MinexAutomation <minex.automation@gmail.com>
2020-06-21 19:18:57 -0700

    Added R5T.Dacia.Extensions (DI extensiions) project-reference dependency. Added IServiceCollection extensions.

---
+++ source/R5T.D0018.I0001/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0018.I0001
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="NewXDocumentVisualStudioProjectFileGenerator"/> implementation of <see cref="INewVisualStudioProjectFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddNewXDocumentVisualStudioProjectFileGenerator(this IServiceCollection services)
+        {
+            services.AddSingleton<INewVisualStudioProjectFileGenerator, NewXDocumentVisualStudioProjectFileGenerator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="NewXDocumentVisualStudioProjectFileGenerator"/> implementation of <see cref="INewVisualStudioProjectFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<INewVisualStudioProjectFileGenerator> AddNewXDocumentVisualStudioProjectFileGeneratorAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<INewVisualStudioProjectFileGenerator>.New(() => services.AddNewXDocumentVisualStudioProjectFileGenerator());
+            return serviceAction;
+        }
+    }
+}
