2b1f1cbd44252a1963e5f5df25d116bf1d0ec09b
MinexAutomation <minex.automation@gmail.com>
2020-06-21 19:14:32 -0700

    Added R5T.D0018.I0001 (XDocumentVisualStudioProjectFile)-based INewVisualStudioProjectFileGenerator implementation. Added R5T.T0004 (XDocumentVisualStudioProjectFile) project.

--- source/R5T.D0018.Base/Code/Services/Definitions/INewVisualStudioProjectFileGenerator.cs
+++ source/R5T.D0018.Base/Code/Services/Definitions/INewVisualStudioProjectFileGenerator.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using R5T.T0002;
 
 
-namespace R5T.T0004.Construction
+namespace R5T.D0018
 {
     /// <summary>
     /// Service to create new <see cref="IVisualStudioProjectFile"/> instances.
---
+++ source/R5T.D0018.I0001/Code/Services/Implementations/NewXDocumentVisualStudioProjectFileGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0002;
+using R5T.T0004;
+
+
+namespace R5T.D0018.I0001
+{
+    public class NewXDocumentVisualStudioProjectFileGenerator : INewVisualStudioProjectFileGenerator
+    {
+        public Task<IVisualStudioProjectFile> CreateNewVisualStudioProjectFile()
+        {
+            var xDocumentVisualStudioProjectFile = XDocumentVisualStudioProjectFile.New();
+
+            return Task.FromResult(xDocumentVisualStudioProjectFile as IVisualStudioProjectFile);
+        }
+    }
+}
